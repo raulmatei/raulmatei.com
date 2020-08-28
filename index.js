e9eba48565a1f87d144ee290dbd4b9378772c6e0
Raul Matei <raul.matei@8x8.com>
2020-08-28 17:00:50 +0300

    Update `babel` dependencies

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-import 'react-hot-loader/patch';
-import 'babel-polyfill'
+import '@babel/polyfill'
 import * as main from './src/main'
 import Container from './src/components/container'
 
 main.initialize(Container)
-
-if (module.hot) {
-  module.hot.accept('./src/components/container', () => {
-    main.initialize(Container)
-  })
-}
