105dcc99b0ac2956c4e5748993b3e5359817e054
Raul Matei <raul.matei@8x8.com>
2020-08-31 17:25:20 +0300

    Update loader styles

--- src/components/activity-indicator.js
+++ src/components/activity-indicator.js
@@ -1,15 +1,37 @@
 import React from 'react'
-import ContentLoader from 'react-content-loader'
 import {getRandomInt} from '../utils'
 
+const ActivityIndicatorWrapper = ({children}) => (
+  <div className='post post-loading'>
+    {children}
+  </div>
+)
+
+const ActivityIndicatorLeft = () => (
+  <div className='post-left gradient'>&nbsp;</div>
+)
+
+const ActivityIndicatorRight = ({children}) => (
+  <div className='post-right'>{children}</div>
+)
+
+const ActivityIndicatorLines = ({widths}) => (
+  <>
+    <div className='post-line post-line-title gradient' style={{top: '12px', width: `${widths[0]}%`}}/>
+    <div className='post-line post-line-normal gradient' style={{top: '24px', width: `${widths[1]}%`}}/>
+    <div className='post-line post-line-normal gradient' style={{top: '32px', width: `${widths[2]}%`}}/>
+    <div className='post-line post-line-normal gradient' style={{top: '40px', width: `${widths[3]}%`}}/>
+  </>
+)
+
 const ActivityIndicatorItem = ({widths}) => (
-  <ContentLoader backgroundOpacity={0.13} foregroundOpacity={0.25} viewBox="0 0 640 108">
-    <rect x="0" y="0" width="108" height="108" />
-    <rect x="124" y="12" width={widths[0]} height="24" />
-    <rect x="124" y="48" width={widths[1]} height="12" />
-    <rect x="124" y="68" width={widths[2]} height="12" />
-    <rect x="124" y="88" width={widths[3]} height="12" />
-  </ContentLoader>
+  <>
+    <ActivityIndicatorLeft/>
+
+    <ActivityIndicatorRight>
+      <ActivityIndicatorLines widths={widths}/>
+    </ActivityIndicatorRight>
+  </>
 );
 
 const ActivityIndicator = () => {
@@ -18,16 +40,16 @@ const ActivityIndicator = () => {
 
   for (let i = 0; i < len; i++) {
     const widths = [
-      getRandomInt(420, 360),
-      getRandomInt(420, 360),
-      getRandomInt(420, 360),
-      getRandomInt(420, 360),
+      getRandomInt(50, 100),
+      getRandomInt(50, 100),
+      getRandomInt(50, 100),
+      getRandomInt(50, 100),
     ]
 
     output.push(
-      <div className='post-loading' key={`loading-${i}}`}>
+      <ActivityIndicatorWrapper key={`loading-${i}}`}>
         <ActivityIndicatorItem widths={widths}/>
-      </div>
+      </ActivityIndicatorWrapper>
     )
   }
 
